58401597b8f22672cae7fa97ae0cf99e25c41b1f
Muhammad Saad <muhammadsaad@Muhammads-MacBook-Air-2.local>
2023-12-19 15:07:31 +0500

    update vector normalization

--- Game/Vector2D.cpp
+++ Game/Vector2D.cpp
@@ -2,29 +2,11 @@
 //Note most of the functions/constructors were implemented in the header because it's cleaner and easier to visualize.
 
 Vector2D Vector2D::normalize() {
-	float magSquared = magnitudeSquared();
-	if (magSquared > 0.0f) {
-		x *= q_sqrt(magSquared);
-		y *= q_sqrt(magSquared);
+	float magStored = magnitude();
+	if (magStored > 0.0f) {
+		x /= magStored;
+		y /= magStored;
 	}
 
 	return *this;
-}
-
-float Vector2D::q_sqrt(float number) {
-	//This is a fast approximation of the square root function.
-	//Source: https://en.wikipedia.org/wiki/Fast_inverse_square_root
-	long i;
-	float x2, y;
-	const float threehalfs = 1.5F;
-
-	x2 = number * 0.5F;
-	y  = number;
-	i  = * ( long * ) &y;                       // evil floating point bit level hacking
-	i  = 0x5f3759df - ( i >> 1 );               // what the fuck?
-	y  = * ( float * ) &i;
-	y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
-	// y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
-
-  	return y;
 }
